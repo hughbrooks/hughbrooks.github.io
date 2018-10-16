6e65c04fc2a3fd457938658fcb6cc599767228b1
hugh brooks <hhmbrooks@gmail.com>
2018-10-16 11:45:27 +1100

    update
    hgghhjb

---
+++ maximum-website/assets/js/main.js
@@ -0,0 +1,190 @@
+$(document).ready(function(){
+	console.log("ready!");
+
+
+	$('.click-about').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.right').addClass('hide');
+  	$('.about').removeClass('hide');
+	});
+
+	$('.click-buy').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.right').addClass('hide');
+  	$('.buy').removeClass('hide');
+	});
+
+	$('.click-essay').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.right').addClass('hide');
+  	$('.essay').removeClass('hide');
+	});
+
+	$('.pzoegays').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.zoegays, .comments-container ').removeClass('hide');
+	});
+
+	$('.pgeometer99').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.geometer99, .comments-container ').removeClass('hide');
+	});
+
+	$('.psevon42').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.sevon42, .comments-container ').removeClass('hide');
+	});
+
+	$('.pmodex').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.modex, .comments-container ').removeClass('hide');
+	});
+
+	$('.pphilmtl').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.philmtl, .comments-container ').removeClass('hide');
+	});
+
+	$('.pnameofuser217').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.nameofuser217, .comments-container ').removeClass('hide');
+	});
+
+	$('.pdb214').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.db214, .comments-container ').removeClass('hide');
+	});
+
+	$('.pdelecti').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.delecti, .comments-container ').removeClass('hide');
+	});
+
+	$('.pincognegros').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.incognegros, .comments-container ').removeClass('hide');
+	});
+
+	$('.pwasabipeanuts').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.wasabipeanuts, .comments-container ').removeClass('hide');
+	});
+
+	$('.pmelesana').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.melesana, .comments-container ').removeClass('hide');
+	});
+
+	$('.pnotspicyenough').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.notspicyenough, .comments-container ').removeClass('hide');
+	});
+
+	$('.pladytreehugger').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.ladytreehugger, .comments-container ').removeClass('hide');
+	});
+
+	$('.pmuddymississippi').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.muddymississippi, .comments-container ').removeClass('hide');
+	});
+
+	$('.pneverbethesky').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.neverbethesky, .comments-container ').removeClass('hide');
+	});
+
+	$('.pshraquoia').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.shraquoia, .comments-container ').removeClass('hide');
+	});
+
+	$('.pthelegendofpict').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.thelegendofpict, .comments-container ').removeClass('hide');
+	});
+
+	$('.pjchriddy').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.jchriddy, .comments-container ').removeClass('hide');
+	});
+
+	$('.pdeleted').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.deleted, .comments-container ').removeClass('hide');
+	});
+
+	$('.pdadwith2sons').click(function() {
+	$('.menu span, .contributers span').removeClass('blue');
+  	$(this).addClass('blue');
+  	$('.comments-container div').addClass('hide');
+  	$('.dadwith2sons, .comments-container ').removeClass('hide');
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dont delete or load jquery here
+});
+
+
+
+
+
